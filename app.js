fdce67bd90b3b8947e1251c2720badb34b7aabab
Daniel-21MM <danielsanz8001@gmail.com>
2023-08-13 15:32:47 -0600

    se implenta sweet alert

--- app.js
+++ app.js
@@ -72,46 +72,54 @@ app.post('/register', async (req, res) => {
 
 
 //11 - Metodo para la autenticacion
-app.post('/auth', async (req, res)=> {
-	const user = req.body.user;
-	const pass = req.body.pass;    
-    let passwordHash = await bcrypt.hash(pass, 8);
-	if (user && pass) {
-		connection.query('SELECT * FROM users WHERE user = ?', [user], async (error, results, fields)=> {
-			if( results.length == 0 || !(await bcrypt.compare(pass, results[0].pass)) ) {    
-				res.render('login', {
-                        alert: true,
-                        alertTitle: "Error",
-                        alertMessage: "USUARIO y/o PASSWORD incorrectas",
-                        alertIcon:'error',
-                        showConfirmButton: true,
-                        timer: false,
-                        ruta: 'login'    
-                    });
-				
-				//Mensaje simple y poco vistoso
-                //res.send('Incorrect Username and/or Password!');				
-			} else {         
-				//creamos una var de session y le asignamos true si INICIO SESSION       
-				req.session.loggedin = true;                
-				req.session.name = results[0].name;
-				res.render('login', {
-					alert: true,
-					alertTitle: "Conexión exitosa",
-					alertMessage: "¡LOGIN CORRECTO!",
-					alertIcon:'success',
-					showConfirmButton: false,
-					timer: 1500,
-					ruta: ''
-				});        			
-			}			
-			res.end();
-		});
-	} else {	
-		res.send('Please enter user and Password!');
-		res.end();
-	}
+app.post('/auth', async (req, res) => {
+    const user = req.body.user;
+    const pass = req.body.pass;
+
+    if (user && pass) {
+        connection.query('SELECT * FROM usuarios WHERE usuario = ?', [user], async (error, results, fields) => {
+            if (error) {
+                console.error(error);
+                return res.status(500).send('Error en la consulta');
+            }
+
+            if (results.length === 0 || !(await bcrypt.compare(pass, results[0].contrasena))) {
+                return res.render('login', {
+                    alert: true,
+                    alertTitle: "Error",
+                    alertMessage: "Usuario y/o contraseña incorrectas",
+                    alertIcon: 'error',
+                    showConfirmButton: true,
+                    timer: false,
+                    ruta: 'login'
+                });
+            } else {
+                req.session.loggedin = true;
+                req.session.name = results[0].nombre;
+                return res.render('login', {
+                    alert: true,
+                    alertTitle: "Conexión exitosa",
+                    alertMessage: "¡Inicio de sesión correcto!",
+                    alertIcon: 'success',
+                    showConfirmButton: false,
+                    timer: 1500,
+                    ruta: ''
+                });
+            }
+        });
+    } else {
+        return res.render('login', {
+            alert: true,
+            alertTitle: "Error",
+            alertMessage: "Por favor, ingresa usuario y contraseña",
+            alertIcon: 'error',
+            showConfirmButton: true,
+            timer: false,
+            ruta: 'login'
+        });
+    }
 });
+
 
 
 
